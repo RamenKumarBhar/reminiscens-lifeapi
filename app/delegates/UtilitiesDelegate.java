b6972eb1875450f47021992d1fb132e1896282bc
Cristhian Parra <cdparra@gmail.com>
2013-08-19 17:48:23 +0200

    fixed file upload and added thumbnail, medium, large support

--- app/delegates/UtilitiesDelegate.java
+++ app/delegates/UtilitiesDelegate.java
@@ -1,11 +1,5 @@
 package delegates;
 
-import java.awt.Graphics2D;
-import java.awt.GraphicsConfiguration;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-import java.awt.Image;
-import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -25,42 +19,48 @@ import pojos.CityBean;
 import pojos.FileBean;
 import utils.FileUtilities;
 import utils.PlayDozerMapper;
+import static org.imgscalr.Scalr.*;
 
 public class UtilitiesDelegate {
+
+	public final String uploadDir = Play.application().configuration()
+			.getString("files.home");
+	public final String filesBaseURL = Play.application().configuration()
+			.getString("files.baseurl");
+
 	public static UtilitiesDelegate getInstance() {
 		return new UtilitiesDelegate();
 	}
 
-	public List<CityBean> getCities () {
-		List<models.City> 
-			modelCities = 
-				models.City.all();
+	public List<CityBean> getCities() {
+		List<models.City> modelCities = models.City.all();
 		List<CityBean> pojosCities = new ArrayList<CityBean>();
 		for (models.City city : modelCities) {
-			CityBean cityBean = PlayDozerMapper.getInstance().map(
-					city, CityBean.class);
+			CityBean cityBean = PlayDozerMapper.getInstance().map(city,
+					CityBean.class);
 			pojosCities.add(cityBean);
 		}
 		return pojosCities;
 	}
-	
+
 	public List<CityBean> getCitiesByCountryId(Long countryId) {
 		List<models.City> modelCities = models.City.readByCountry(countryId);
 		List<CityBean> pojosCities = new ArrayList<CityBean>();
 		for (models.City city : modelCities) {
-			CityBean cityBean = PlayDozerMapper.getInstance().map(
-					city, CityBean.class);
+			CityBean cityBean = PlayDozerMapper.getInstance().map(city,
+					CityBean.class);
 			pojosCities.add(cityBean);
 		}
 		return pojosCities;
 	}
 
 	public List<CityBean> getCitiesByCountryName(String countryName) {
-		List<models.City> modelCities = models.City.readByCountryName(countryName);
+		List<models.City> modelCities = models.City
+				.readByCountryName(countryName);
 		List<CityBean> pojosCities = new ArrayList<CityBean>();
 		for (models.City city : modelCities) {
-			CityBean cityBean = PlayDozerMapper.getInstance().map(
-					city, CityBean.class);
+			CityBean cityBean = PlayDozerMapper.getInstance().map(city,
+					CityBean.class);
 			pojosCities.add(cityBean);
 		}
 		return pojosCities;
@@ -68,34 +68,34 @@ public class UtilitiesDelegate {
 
 	public List<CityBean> getCitiesByCountryNameAndRegion(String country,
 			String region) {
-		List<models.City> modelCities = models.City.readByCountryNameAndRegion(country, region);
+		List<models.City> modelCities = models.City.readByCountryNameAndRegion(
+				country, region);
 		List<CityBean> pojosCities = new ArrayList<CityBean>();
 		for (models.City city : modelCities) {
-			CityBean cityBean = PlayDozerMapper.getInstance().map(
-					city, CityBean.class);
-			pojosCities.add(cityBean);
-		}
-		return pojosCities;	
-	}
-
-	
-	public List<CityBean> getNewCities (Long lastCityId) {
-		List<models.City> modelCities = models.City.readNewById(lastCityId);
-		List<CityBean> pojosCities = new ArrayList<CityBean>();
-		for (models.City city : modelCities) {
-			CityBean cityBean = PlayDozerMapper.getInstance().map(
-					city, CityBean.class);
+			CityBean cityBean = PlayDozerMapper.getInstance().map(city,
+					CityBean.class);
 			pojosCities.add(cityBean);
 		}
 		return pojosCities;
 	}
-	
-	public List<CityBean> getCityByName (String cityName) {
+
+	public List<CityBean> getNewCities(Long lastCityId) {
+		List<models.City> modelCities = models.City.readNewById(lastCityId);
+		List<CityBean> pojosCities = new ArrayList<CityBean>();
+		for (models.City city : modelCities) {
+			CityBean cityBean = PlayDozerMapper.getInstance().map(city,
+					CityBean.class);
+			pojosCities.add(cityBean);
+		}
+		return pojosCities;
+	}
+
+	public List<CityBean> getCityByName(String cityName) {
 		List<models.City> modelCities = models.City.findByName(cityName);
 		List<CityBean> pojosCities = new ArrayList<CityBean>();
 		for (models.City city : modelCities) {
-			CityBean cityBean = PlayDozerMapper.getInstance().map(
-					city, CityBean.class);
+			CityBean cityBean = PlayDozerMapper.getInstance().map(city,
+					CityBean.class);
 			pojosCities.add(cityBean);
 		}
 		return pojosCities;
@@ -103,31 +103,32 @@ public class UtilitiesDelegate {
 
 	public CityBean getCitiesById(Long cityId) {
 		models.City city = models.City.read(cityId);
-		CityBean cityBean = PlayDozerMapper.getInstance().map(city, CityBean.class);
+		CityBean cityBean = PlayDozerMapper.getInstance().map(city,
+				CityBean.class);
 		return cityBean;
-	}	
+	}
 
-	public FileBean saveFile(FilePart file, FileBean fileBean) throws IOException {
+	public FileBean saveFile(FilePart file, FileBean fileBean)
+			throws IOException {
 		/*
-		 * 1. Prepare file metadata before saving the file in the final destination
+		 * 1. Prepare file metadata before saving the file in the final
+		 * destination
 		 */
-		String uploadDir = Play.application().configuration().getString("files.home");
-		String filesBaseURL = Play.application().configuration().getString("files.baseurl");
-		
-		String fileName = "U_"+fileBean.getOwner().toString()+"_"+"D"+DateTime.now()+"_"+file.getFilename();
+
+		String fileName = "U_" + fileBean.getOwner().toString() + "_" + "D"
+				+ DateTime.now() + "_" + file.getFilename();
 		// TODO check how to make uploadDir also cross-platform
-		String fullPath = uploadDir+ FileUtilities.slash + fileName;
+		String fullPath = uploadDir + FileUtilities.slash + fileName;
 		String contentType = file.getContentType();
 		File uploadFile = file.getFile();
 
-		String [] parts = file.getFilename().split(".");
+		String[] parts = contentType.split("/");
 		String fileExtension = "";
-		
+
 		if (parts != null && parts.length > 0) {
-			fileExtension = parts[parts.length-1];	
+			fileExtension = parts[parts.length - 1];
 		}
-		
-		
+
 		Logger.root().debug("Saving File....");
 		Logger.root().debug("--> fileName=" + fileName);
 		Logger.root().debug("--> contentType=" + contentType);
@@ -137,78 +138,109 @@ public class UtilitiesDelegate {
 		 * 2. Save the file in the final destination
 		 */
 		File localFile = new File(fullPath);
-		uploadFile.renameTo(localFile);		
+		uploadFile.renameTo(localFile);
 		Logger.root().debug("--> localFile=" + localFile);
+		
+		
+		/*
+		 * 3. Save scaled down versions
+		 */
 		File thumbnailFile = null;
-		
-		if (contentType.contains("image"))
-			thumbnailFile = scalePicture(localFile);
-		
+		File mediumFile = null;
+		File largeFile = null;
+
+		if (contentType.contains("image")) {
+			thumbnailFile = scalePictureAndSave(localFile, fileExtension, "THUMBNAIL");
+			mediumFile = scalePictureAndSave(localFile, fileExtension, "MEDIUM");
+			largeFile = scalePictureAndSave(localFile, fileExtension, "LARGE");
+		}
+
 		String thumbnailURI = "";
+		String mediumURI = "";
+		String largeURI = "";
 		if (thumbnailFile != null) {
 			thumbnailURI = filesBaseURL + "/" + thumbnailFile.getName();
 		}
+
+		if (mediumFile != null) {
+			mediumURI = filesBaseURL + "/" + mediumFile.getName();
+		}
+
+		if (largeFile != null) {
+			largeURI = filesBaseURL + "/" + largeFile.getName();
+		}
+		
 		/*
 		 * 3. Generate Hashcode to use as new name
 		 */
-		String hashcode = UUID.nameUUIDFromBytes(fullPath.getBytes()).toString();
-		/* 
+		String hashcode = UUID.nameUUIDFromBytes(fullPath.getBytes())
+				.toString();
+		/*
 		 * 4. Save File metadata in Database
 		 */
 		fileBean.setFilename(fileName);
 		fileBean.setURI(filesBaseURL + "/" + fileName);
 		fileBean.setThumbnailURI(thumbnailURI);
+		fileBean.setMediumURI(mediumURI);
+		fileBean.setLargeURI(largeURI);
 		fileBean.setContentType(contentType);
 		fileBean.setCreationDate(DateTime.now());
 		fileBean.setHashcode(hashcode);
 		fileBean.setExtension(fileExtension);
 		Logger.root().debug("--> creationDate=" + fileBean.getCreationDate());
 		Logger.root().debug("--> hashcode=" + fileBean.getHashcode());
-		
-		models.File f = PlayDozerMapper.getInstance().map(fileBean, models.File.class);
+
+		models.File f = PlayDozerMapper.getInstance().map(fileBean,
+				models.File.class);
 		f.save();
 		fileBean.setFileId(f.getFileId());
 		return fileBean;
 	}
-	
-	
-	private File scalePicture(File localFile) throws IOException {
-		BufferedImage img = new BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
-		img.createGraphics().drawImage(ImageIO.read(localFile), null, null);
-		BufferedImage scaled = scale(img,0.5);
-		File thumbFile = new File(localFile.getAbsolutePath()+"_thumb");
-		ImageIO.write(scaled, "jpg", thumbFile);
+
+	private File scalePictureAndSave(File sourceImageFile, String fileExtension, String size)
+			throws IOException {
+
+		// 1. Read image
+		BufferedImage img = ImageIO.read(sourceImageFile);
+
+		// 2. Create scaled image
+		BufferedImage scaled = null;
+		if (size.equals("THUMBNAIL")) {
+			// thumbnail=150px 
+			scaled = FileUtilities.createThumbnail(img);
+		} else if (size.equals("SMALL")) {
+			// small=250px
+			scaled = FileUtilities.createSmall(img);
+		} else if (size.equals("MEDIUM")) {
+			// medium=500px
+			scaled = FileUtilities.createMedium(img);
+		} else {
+			// large=1024px
+			scaled = FileUtilities.createLarge(img);
+		}
+		
+		scaled.flush();
+		
+		// 3. prepare the path and filename for the scaled image
+		String fileName = size+"_" + sourceImageFile.getName();
+		String fullPath = uploadDir + FileUtilities.slash + fileName;
+		File thumbFile = new File(fullPath);
+		
+		// 4. save scaled image
+		ImageIO.write(scaled, fileExtension, thumbFile);
 		return thumbFile;
 	}
 
 	public File getFile(String hashcode, Long userId) {
 		models.File f = models.File.readByHashCodeAndUserId(hashcode, userId);
-		
-		String uploadDir = Play.application().configuration().getString("files.home");
+
+		String uploadDir = Play.application().configuration()
+				.getString("files.home");
 		String fileName = f == null ? "" : f.getFilename();
-		String fullPath = f == null ? "" : uploadDir+ FileUtilities.slash + fileName;
-	
+		String fullPath = f == null ? "" : uploadDir + FileUtilities.slash
+				+ fileName;
+
 		return new File(fullPath);
 	}
-	
-	private BufferedImage scale(BufferedImage source,double ratio) {
-		  int w = (int) (source.getWidth() * ratio);
-		  int h = (int) (source.getHeight() * ratio);
-		  BufferedImage bi = getCompatibleImage(w, h);
-		  Graphics2D g2d = bi.createGraphics();
-		  double xScale = (double) w / source.getWidth();
-		  double yScale = (double) h / source.getHeight();
-		  AffineTransform at = AffineTransform.getScaleInstance(xScale,yScale);
-		  g2d.drawRenderedImage(source, at);
-		  g2d.dispose();
-		  return bi;
-		}
 
-	private BufferedImage getCompatibleImage(int w, int h) {
-		  GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-		  GraphicsDevice gd = ge.getDefaultScreenDevice();
-		  GraphicsConfiguration gc = gd.getDefaultConfiguration();
-		  BufferedImage image = gc.createCompatibleImage(w, h);
-		  return image;
-		}
 }
--- app/utils/FileUtilities.java
+++ app/utils/FileUtilities.java
@@ -1,12 +1,15 @@
 package utils;
 
-import java.io.BufferedWriter;
+import static org.imgscalr.Scalr.OP_ANTIALIAS;
+import static org.imgscalr.Scalr.OP_BRIGHTER;
+import static org.imgscalr.Scalr.resize;
+
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+
+import org.imgscalr.Scalr.Method;
 
 public class FileUtilities {
-
 
 	/**
 	 * This corresponds to the delimiter of the file path for the current
@@ -18,6 +21,39 @@ public class FileUtilities {
     public static File readFile(String fullPath) {
     	return new File(fullPath);
     }
+    
+	public static BufferedImage createThumbnail(BufferedImage img) {
+		// Create quickly, then smooth and brighten it.
+		img = resize(img, Method.SPEED, 125, OP_ANTIALIAS, OP_BRIGHTER);
+
+		// Let's add a little border before we return result.
+		return img;
+	}
+	
+	public static BufferedImage createLarge(BufferedImage img) {
+		// Create quickly, then smooth and brighten it.
+		img = resize(img, Method.SPEED, 1024, OP_ANTIALIAS, OP_BRIGHTER);
+
+		// Let's add a little border before we return result.
+		return img;
+	}
+	
+	public static BufferedImage createMedium(BufferedImage img) {
+		// Create quickly, then smooth and brighten it.
+		img = resize(img, Method.SPEED, 500, OP_ANTIALIAS, OP_BRIGHTER);
+
+		// Let's add a little border before we return result.
+		return img;
+	}	
+
+	public static BufferedImage createSmall(BufferedImage img) {
+		// Create quickly, then smooth and brighten it.
+		img = resize(img, Method.SPEED, 250, OP_ANTIALIAS, OP_BRIGHTER);
+
+		// Let's add a little border before we return result.
+		return img;
+	}	
+
     
     public static String generateHashCode(String fullPath, Long userId, String contentType) {
     	// TODO implement hash code for files generation
