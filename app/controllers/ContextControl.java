97b8a74302414c4e9fa0fb13c3708aa538184737
Cristhian Parra <cdparra@gmail.com>
2013-08-21 16:13:58 +0200

    updated routes for context, updated controller with skeleton of services

--- app/controllers/ContextControl.java
+++ app/controllers/ContextControl.java
@@ -3,56 +3,86 @@ package controllers;
 import play.mvc.*;
 
 public class ContextControl extends Controller {
+	public static Result initContextForPersonAndDecadeAndCity(Long id, Long decade,
+			Long cityId) {
+		/** @TODO */
+		return TODO;
+	}
+
+	public static Result initContextForPersonAndDecade(Long id, Long decade) {
+		/** @TODO */
+		return TODO;
+	}
+
+	public static Result initContextForPerson(Long id) {
+		/** @TODO */
+		return TODO;
+	}
+
+	public static Result refreshContextForDecadeAndCity(Long cid, Long decade,
+			Long cityId) {
+		/** @TODO */
+		return TODO;
+	}
+
+	public static Result refreshContextForDecade(Long cid, Long decade) {
+		/** @TODO */
+		return TODO;
+	}
+
+	public static Result refreshContext(Long cid) {
+		/** @TODO */
+		return TODO;
+	}
 
 	public static Result getContext(Long cid) {
-    	/** @TODO */
+		/** @TODO */
 		return TODO;
 	}
 
-	public static Result getContextCurators(Long cid) {
-    	/** @TODO */
+	public static Result getContextForDecade(Long cid, Long decade) {
+		/** @TODO */
 		return TODO;
 	}
 
-	public static Result getContextMemento(Long cid, Long mid) {
-    	/** @TODO */
+	public static Result getContextForDecadeAndCity(Long cid, Long decade,
+			Long cityId) {
+		/** @TODO */
+		return TODO;
+	}
+
+	public static Result getContextFilterCategory(Long cid, String cat) {
+		/** @TODO */
 		return TODO;
 	}
 
 	public static Result createContextMemento(Long cid) {
-    	/** @TODO */
+		/** @TODO */
 		return TODO;
 	}
 
-	public static Result updateContextMemento(Long cid) {
-    	/** @TODO */
+	public static Result updateContextMemento(Long cid, Long mid) {
+		/** @TODO */
 		return TODO;
 	}
 
 	public static Result deleteContextMemento(Long cid, Long mid) {
-    	/** @TODO */
+		/** @TODO */
 		return TODO;
 	}
 
-	public static Result getContextQuestions(Long cid) {
-    	/** @TODO */
+	public static Result getContextCurators(Long cid) {
+		/** @TODO */
 		return TODO;
 	}
 
-	public static Result getContextQuestionsForDecade(Long cid, Long decade) {
-    	/** @TODO */
+	public static Result getContextMemento(Long cid, Long mid) {
+		/** @TODO */
 		return TODO;
 	}
 
-	public static Result getContextQuestionsByCurators(Long cid) {
-    	/** @TODO */
+	public static Result initContext() {
+		/** @TODO */
 		return TODO;
 	}
-
-	public static Result createContextCuratedQuestions(Long cid) {
-    	/** @TODO */
-		return TODO;
-	}
-
-  
 }
