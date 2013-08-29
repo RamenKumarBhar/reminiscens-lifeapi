72532f0d936d1c10e19528b4809218fa9bda0de4
Cristhian Parra <cdparra@gmail.com>
2013-08-29 22:51:24 +0200

    minor updates, documentation update in apiary

--- app/models/Context.java
+++ app/models/Context.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.persistence.*;
 
 import org.codehaus.jackson.annotate.JsonIgnore;
-import org.hibernate.annotations.IndexColumn;
 import org.hibernate.annotations.Type;
 import org.joda.time.DateTime;
 
